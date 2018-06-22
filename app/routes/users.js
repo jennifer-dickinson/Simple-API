97ef9ec30aeed4ad1a72a7dd7dfe60b6b30c20f4
jennifersalas <jennysalas@me.com>
2018-06-21 21:09:13 -0700

    User signup/login endpoint present. Need validation, salting, and database integration.

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,24 +1,33 @@
+function signup(req, res) {
+    var email = req.body['email'];
+    var password = req.body['password'];
+
+    //Valide email
+    req.checkBody('email', 'Email is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    // req.checkBody('email', 'User already exists').custom();
+
+    req.checkBody('password', 'Password is required').notEmpty();
+
+    //Check email
+    
+
+    var errors =  req.validationErrors();
+    if (errors) {
+        res.status(400).send('{ error: ' + JSON.stringify(errors[0]['msg']) + '}');
+    }
+    else {
+         res.status(200).send('{ msg: "Valid part signup needs to be implemented" }');
+        // res.status(200).send('{session: <token> }\n');
+    }
+}
+
+function login(req, res) {
+    res.status(200).send('{ msg: "Need to implement this" }\n');
+}
+
 module.exports = function(app, db){
-    app.post('/user/signup', (req, res) => {
-        var email = req.body['email'];
-        var password = req.body['password'];
-        // console.log(email, password);
+    app.post('/user/signup', (req, res) => signup(req, res));
+    app.post('/user/login', (req, res) => login(req, res));
 
-        //Validation
-        req.checkBody('email', 'Email is required').notEmpty();
-        req.checkBody('email', 'Email is not valid').isEmail();
-        req.checkBody('password', 'Password is required').notEmpty();
-
-
-        var errors =  req.validationErrors();
-
-        if (errors) {
-            console.log('There are errors');
-            console.log(errors);
-        } else {
-            console.log('No errors');
-        }
-
-        res.send('{email: ' + email +', password: ' + password + '}');
-    });
 };
